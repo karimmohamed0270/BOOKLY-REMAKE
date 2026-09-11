4aad8855d7403238f375e1d28036e5750b1ce81a
karimmohamed0270 <karimmohamed070@gmail.com>
2026-09-11 17:35:56 +0300

    best seller item done

--- lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -9,14 +9,16 @@ class BestSellerListViewItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 125,
+      height: 100,
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // image
           AspectRatio(
-            aspectRatio: 2.5 / 4,
+            aspectRatio: 2.3 / 4,
             child: Container(
               decoration: BoxDecoration(
-                image: DecorationImage(
+                image: const DecorationImage(
                   image: AssetImage(AssetsData.testImage),
                   fit: BoxFit.fill,
                 ),
@@ -24,14 +26,15 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(width: 30),
 
-          Padding(
-            padding: const EdgeInsets.only(left: 30, top: 2),
+          // book details (title, author, price and rating)
+          Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
-                  // to avoid the the proble  of the text m o sized
+                  // to avoid the problem of text overflow
                   width: MediaQuery.of(context).size.width * .5,
                   child: Text(
                     "The Jungle Book",
@@ -43,10 +46,32 @@ class BestSellerListViewItem extends StatelessWidget {
                 const SizedBox(height: 3),
                 Text(
                   "J.K Rowling",
-                  style: Styles.textStyle16.copyWith(
-                    color: Colors.grey,
-                    // fontFamily: GoogleFonts.cairo().fontFamily,
-                  ),
+                  style: Styles.textStyle16.copyWith(color: Colors.grey),
+                ),
+
+                // price and rating
+                SizedBox(height: 12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      r"19.99 €",
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(right: 60.0),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.yellow, size: 18),
+                          SizedBox(width: 4),
+                          Text("4.8", style: Styles.textStyle16),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
